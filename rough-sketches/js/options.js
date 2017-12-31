0cf87dbb71545f53691aa1ba3b6b0da20942bc31
mram <mram@umd.edu>
2017-12-31 02:34:14 -0500

    Completed a lot of front end

--- rough-sketches/js/options.js
+++ rough-sketches/js/options.js
@@ -1,13 +1,18 @@
 $(document).ready(function() {
-    $('#latest-copy').click(function() {
+    $('.copy').click(function() {
+
+        // console.log("banana");
+        let id = '#' + $(this).attr('id').slice(0, -5);
+        // console.log(id);
 
         let temp = document.createElement('input');
-        temp.setAttribute('value', $('#latest-quote').text());
+        temp.setAttribute('value', $(id).find($('.quote')).text());
         document.body.appendChild(temp);
         temp.select();
 
         document.execCommand("copy");
 
         document.body.removeChild(temp);
+
     });
 });
