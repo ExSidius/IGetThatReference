013561a842ad9775fd917560e34e545444206bd1
mram <mram@umd.edu>
2017-12-31 03:28:02 -0500

    Created post model

--- references/models.py
+++ references/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+import json
 
 # Create your models here.
+class Reference(models.Model):
+
+    quote = models.TextField()
+    author = models.CharField(max_length=100)
+    source = models.CharField(max_length=100)
+    pub_date = models.DateTimeField()
+    tags = models.TextField()
+
+    def setTags(self, tags):
+        self.tags = json.dumps(tags)
+
+    def getTags(self):
+        return json.loads(self.tags)
