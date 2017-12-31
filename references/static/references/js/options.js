a3637f12d3d3e4f28748034b6b3d87d08ec4fc8c
mram <mram@umd.edu>
2017-12-31 16:23:00 -0500

    Fixed copy functionality

--- references/static/references/js/options.js
+++ references/static/references/js/options.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
     $('.copy').click(function() {
 
-        // console.log("banana");
-        let id = '#' + $(this).attr('id').slice(0, -5);
-        // console.log(id);
+        console.log("banana");
+        let id = '#' + 'quote' + $(this).attr('id').slice(4);
+        console.log(id);
 
         let temp = document.createElement('input');
         temp.setAttribute('value', $(id).find($('.quote')).text());
