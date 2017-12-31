e04f8b35baeb0a97c3378c66b50bffbea4f502f8
mram <mram@umd.edu>
2017-12-31 16:23:39 -0500

    Removed debugging statements

--- references/static/references/js/options.js
+++ references/static/references/js/options.js
@@ -1,9 +1,7 @@
 $(document).ready(function() {
     $('.copy').click(function() {
 
-        console.log("banana");
         let id = '#' + 'quote' + $(this).attr('id').slice(4);
-        console.log(id);
 
         let temp = document.createElement('input');
         temp.setAttribute('value', $(id).find($('.quote')).text());
