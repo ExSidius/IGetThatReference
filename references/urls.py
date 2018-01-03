f76c2f26c06b7de06af15ed3481ebfaa8cf29bb5
mram <mram@umd.edu>
2018-01-03 00:25:43 -0500

    Set up tag and search pages

--- references/urls.py
+++ references/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'references'
 
 urlpatterns = [
-    path('search/', views.Search.as_view(), name='search'),
+    re_path('^tag/(?P<tag_name>[\w\W\s]+)/', views.tag, name='tag'),
+    re_path('^search/(?P<search_param>[\w\W\s]+)/', views.search, name='search'),
 ]
--- references/views.py
+++ references/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.views import View
+from django.views.generic import TemplateView, ListView, DetailView
 from .models import Reference
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
@@ -9,8 +10,32 @@ def home(request):
     references = Reference.objects.order_by('-pub_date')
     tags = [list(map(str.strip, reference.tags.split(','))) for reference in references]
 
-    return render(request, 'references/home.html', {'info': zip(references, tags)})
+    context = {'info': zip(references, tags)}
 
-class Search(View):
-    def get(self, request):
-        return HttpResponse("OOOOH Shit")
+    return render(request, 'references/home.html', context)
+
+def tag(request, tag_name):
+
+    references = [reference for reference in Reference.objects.order_by('-pub_date') if tag_name in reference.tags]
+    tags = [list(map(str.strip, reference.tags.split(','))) for reference in references]
+
+    context = {
+                'type': 'tag',
+                'info': zip(references, tags),
+                'input': tag_name
+            }
+
+    return render(request, 'references/tagsearch.html', context)
+
+def search(request, search_param):
+
+    references = [reference for reference in Reference.objects.order_by('-pub_date') if search_param in reference.tags]
+    tags = [list(map(str.strip, reference.tags.split(','))) for reference in references]
+
+    context = {
+                'type': 'search',
+                'info': zip(references, tags),
+                'input': search_param
+            }
+
+    return render(request, 'references/tagsearch.html', context)
